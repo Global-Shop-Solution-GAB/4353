8123e396a91ff7a5a0bb937e611fed48fdf6ab70
Service Web <sw@gssmail.com>
2024-03-11 16:18:55 -0500

    Commit changes in C:\Websites\Git\emarlow\4353\GCG_4353_Ord_Header.sql

---
+++ GCG_4353_Ord_Header.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "GCG_4353_Ord_Header"(
+ "Order_No" VARCHAR(7) NOT NULL ,
+ "Branch" VARCHAR(9) NOT NULL ,
+ "Customer" VARCHAR(6),
+ "Taker" VARCHAR(8),
+ "Taker_Name" VARCHAR(30),
+ PRIMARY KEY ("Order_No"));
