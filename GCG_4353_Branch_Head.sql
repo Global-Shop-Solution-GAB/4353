c6e579b883f0456aa510ca9a27ce07baa24bedf5
Service Web <sw@gssmail.com>
2024-03-11 16:18:55 -0500

    Commit changes in C:\Websites\Git\emarlow\4353\GCG_4353_Branch_Head.sql

---
+++ GCG_4353_Branch_Head.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "GCG_4353_Branch_Head"(
+ "Customer" VARCHAR(9) NOT NULL ,
+ "Customer_Name" VARCHAR(225),
+ "Address1" VARCHAR(225),
+ "Address2" VARCHAR(225),
+ "City" VARCHAR(225),
+ "State" VARCHAR(225),
+ "Zip" VARCHAR(225),
+ "Country" VARCHAR(225));
